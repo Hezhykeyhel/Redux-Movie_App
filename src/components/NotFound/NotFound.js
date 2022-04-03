8bffa6ade019051f1df1d6066c1e06949d1ffe8b
Hezhykeyhel <damilolaegbetayo12@gmail.com>
2022-04-03 13:46:50 +0100

    Updated Stylings for overflows and reduced fonts for smaller screen sizes

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import notfound from '../../images/not_found.png'
+import './NotFound.scss'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div>
+      <h4>The page you're looking for does not exist</h4>
+      <img src={notfound} alt='Not_found'/>
+    </div>
   )
 }
 
